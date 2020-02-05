e03692e3cc4a4c9074f548b97204046813a46169
kowthalganeshk <kowdal.ganeshkumar@iff.com>
2020-02-05 14:41:07 +0530

    Updates

---
+++ casbin-angular/main-es2015.js
@@ -0,0 +1,27 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["main"],{
+
+/***/ "./src/main.ts":
+/*!*********************!*\
+  !*** ./src/main.ts ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+
+/***/ 0:
+/*!***************************!*\
+  !*** multi ./src/main.ts ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! C:\dev\poc\casbin-angular\src\main.ts */"./src/main.ts");
+
+
+/***/ })
+
+},[[0,"runtime"]]]);
+//# sourceMappingURL=main-es2015.js.map
